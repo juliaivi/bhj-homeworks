c711768c0764c3ccfa1baf5680ac97aedd524c3c
juliaivi <juliashinobi@mail.ru>
2023-03-02 11:49:46 +0300

    changes task.js lessons 2.2.

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,16 +2,21 @@ let menuLink = Array.from(document.querySelectorAll(".menu__link"));
 
 menuLink.forEach((el) => {
     el.onclick = () => {
-        let parent = el.closest('.menu__item');
-        let daughter = parent.querySelector('.menu');
-        let menu = el.closest('.menu');
-        let listMenu = menu.querySelectorAll('.menu');
-        
-        for (let i = 0; i < listMenu.length; i++) {
-            listMenu[i].classList.remove('menu_active');
+        let menu = el.closest('.menu__item').querySelector('.menu');
+        let listMenu = el.closest('.menu').querySelectorAll('.menu');
+
+    listMenu.forEach((el) => {
+        if (el !== menu) {
+            el.classList.remove('menu_active');
         }
-        
-        daughter.classList.add('menu_active');
-        return false;
+    })  
+    
+    if (menu == null) {
+        return true;
+    }
+      
+    menu.classList.toggle('menu_active');   
+    return false;          
     }
 })
+
