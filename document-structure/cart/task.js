175e426ed5380f739138f531563fe38e50fa3f8e
juliaivi <juliashinobi@mail.ru>
2023-03-25 11:37:46 +0300

    chande in the 2 and 3 tasks

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -2,8 +2,54 @@ let productControls = Array.from(document.querySelectorAll('.product__quantity-c
 let cartProduct = document.querySelector(".cart__products");
 let productAdd = document.querySelectorAll(".product__add");
 let endPoint;
+let startPoint;
+
+let mus = [];
+
+let statusInit = initProductList();
+
+let generateCartProduct = (imgSrc, dataId, pastValue) => {
+    return `<div class="cart__product" data-id= ${dataId}>
+                <img class="cart__product-image" src =${imgSrc}>
+                <div class="cart__product-count">${pastValue}</div>
+                <div class="product__quantity-controls">
+                    <button class="cart__product_remove">Удaлить</button>
+                </div>
+            </div>`
+}
 
 changeCount(productControls);
+
+cartProduct.addEventListener("click", remuveCart);
+
+drawProduct(mus);
+
+productAdd.forEach((el) => {
+     el.addEventListener("click", addCart);
+})
+
+function initProductList() {
+    let initProduct;
+
+    if (localStorage.length !== undefined || localStorage.length !== 0 || localStorage.length !== null) {
+        document.querySelector(".cart").style.visibility = "visible";
+    }
+
+    let cart = localStorage.getItem('cart');
+    if (cart === "" || cart === null || cart === undefined) {
+        return false;
+    }
+
+    initProduct = JSON.parse(cart);
+
+    if (initProduct === null || initProduct == undefined) {
+        return false;
+    }
+
+    mus = initProduct;
+
+    return true;
+}
 
 function changeCount(elem) {
     elem.forEach((el) => {
@@ -14,104 +60,126 @@ function changeCount(elem) {
             if (event.target.classList.contains('product__quantity-control_inc')) {
                 count++;
                 productValue.innerText = count;
-            } else { 
+            } else {
                 if (count > 1) {
                     count--;
                     productValue.innerText = count;
-                } 
+                }
             }
         })
     })
 }
 
-if (localStorage.length !== undefined) {
-    document.querySelector(".cart").style.visibility = "visible";
-}
+function searchCart(cartProduct, dataId) {
+    let cartProductList = Array.from(cartProduct.querySelectorAll('.cart__product'));
 
-const generateCartProduct = (imgSrc, dataId, pastValue) => {
-    return `<div class="cart__product" data-id= ${dataId}>
-                <img class="cart__product-image" src =${imgSrc}>
-                <div class="cart__product-count">${pastValue}</div>
-                <div class="product__quantity-controls">
-                    <button class="cart__product_remove">Удaлить</button>
-                </div>
-            </div>`
-}
-
-function searchCart(elems, dataId) {
-    for (let elem of elems.children) {
-        if (elem.dataset.id === dataId) {
+    return cartProductList.find((elem) => {
+        if (elem.dataset.id == dataId) {
             return elem;
-        } 
-    }
+        }
+    })
 }
 
 function endPositionElement(elem) {
     endPoint = {
-        x: elem.left, 
-        y: elem.top
-    };   
+        x: elem.left,
+        y: elem.top,
+    };
+}
+
+function startPoints(elem, elem1) {
+    startPoint = {
+        x: elem.left,
+        y: elem.top,
+    }
+
+    elem1.style.position = 'fixed';
+    elem1.style.left = startPoint.x + 'px';
+    elem1.style.top = startPoint.y + 'px';
+    
+    document.querySelector('.cart').appendChild(elem1);
+}
+
+
+function drawProduct(elem) {
+    if (elem == null || elem == undefined) {
+        return;
+    }
+
+    for (let i = 0; i < elem.length; i++) {
+        let localDataId = elem[i].dataId;
+        let localImgSrc = elem[i].imgSrc;
+        let localPastValue = elem[i].pastValue;
+        
+        cartProduct.insertAdjacentHTML("afterbegin", generateCartProduct(localImgSrc, localDataId, localPastValue));
+    }
 }
 
 function addCart(event) {
     let product = event.target.closest(".product");
-    let dataId = product.dataset.id; 
+    let dataId = product.dataset.id;
     let pastValue = +product.querySelector(".product__quantity-value").innerText;
-    let productImg = product.querySelector(".product__image")
+    let productImg = product.querySelector(".product__image");
+    let startPointImg = productImg.getBoundingClientRect();
     let imgSrc = productImg.getAttribute("src");
 
-    let flyingImage = productImg.cloneNode(false); 
+    let flyingImage = productImg.cloneNode(false);
   
+    let mapValue = {
+        dataId: dataId,
+        imgSrc: imgSrc,
+        pastValue: pastValue,
+    }
+
+    if (cartProduct.children) {
+        document.querySelector(".cart").style.visibility = "visible";
+    }
+
     let cart = searchCart(cartProduct, dataId);
     
     if (cart !== undefined) {
         let cartProductCount = cart.querySelector(".cart__product-count");
         let endPosition = cart.querySelector(".cart__product-image").getBoundingClientRect();
-        
-        cartProductCount.innerText = +cartProductCount.innerText + pastValue; 
+
+        cartProductCount.innerText = +cartProductCount.innerText + pastValue;
         endPositionElement(endPosition);
-        localStorage.setItem("cart", cartProduct.innerHTML);
+
+        for (let i = 0; i < mus.length; i++){
+            if(mus[i].dataId == dataId) {
+                mus[i].pastValue = cartProductCount.innerText;
+            }
+        }
+
+        startPoints(startPointImg, flyingImage);
+        animateElementToElement(flyingImage, endPoint, 7, 100);
+        localStorage.setItem("cart", JSON.stringify(mus));
+
         return;
     }
 
-    cartProduct.insertAdjacentHTML("afterbegin", generateCartProduct(imgSrc, dataId, pastValue)); 
+    cartProduct.insertAdjacentHTML("afterbegin", generateCartProduct(imgSrc, dataId, pastValue));
     
-    if (cartProduct.children) {
-        document.querySelector(".cart").style.visibility = "visible"; 
-    }  
-
+    mus.push(mapValue);
+   
+    startPoints(startPointImg, flyingImage);
     if (endPoint == undefined) {
         endPosition = cartProduct.querySelector(".cart__product-image").getBoundingClientRect();
         endPositionElement(endPosition);
     }
- 
-    const startPoint = {
-        x: productImg.getBoundingClientRect().left, 
-        y: productImg.getBoundingClientRect().top        
-    }    
 
-    flyingImage.style.position = 'fixed';
-    flyingImage.style.left = startPoint.x + 'px';
-    flyingImage.style.top = startPoint.y + 'px';     
-    document.querySelector('.cart').appendChild(flyingImage);
     animateElementToElement(flyingImage, endPoint, 7, 100);
-
-    localStorage.setItem("cart", cartProduct.innerHTML);
+    localStorage.setItem("cart", JSON.stringify(mus));
 }
-
-productAdd.forEach((el) => {
-     el.addEventListener("click", addCart);
-})
 
 function animateElementToElement(elem, target, nSteps, tStep) {
     if (nSteps === 1) {
-        elem.remove(); 
-        return;   
-    } 
+        elem.remove();
+        return;
+    }
 
     const point = {
-        x: elem.getBoundingClientRect().left, 
-        y: elem.getBoundingClientRect().top    
+        x: elem.getBoundingClientRect().left,
+        y: elem.getBoundingClientRect().top,
     }
   
     point.x += (target.x - point.x) / nSteps;
@@ -123,17 +191,24 @@ function animateElementToElement(elem, target, nSteps, tStep) {
     setTimeout(() => animateElementToElement(elem, target, nSteps - 1, tStep), tStep);
 }
 
-cartProduct.addEventListener("click", remuveCart);
-
 function remuveCart(event) {
     let productRemove = event.target.closest(".cart__product_remove");
-    let remuveCart = productRemove.closest(".cart__product").remove();  
-     if (!cartProduct.children.length) {
-        document.querySelector(".cart").style.visibility = "hidden"; 
-    } 
+
+    let remuveCart = productRemove.closest(".cart__product");
+    let dataIdRemuveCart = remuveCart.dataset.id;
+
+    remuveCart.remove();
+
+    let index = mus.findIndex((el) => el.dataId === dataIdRemuveCart);
+    if (index !== -1) {
+        mus.splice(index, 1);
+    }
+
+    if (!cartProduct.children.length) {
+        document.querySelector(".cart").style.visibility = "hidden";
+    }
     
-    localStorage.setItem("cart", cartProduct.innerHTML);
+    localStorage.setItem("cart", JSON.stringify(mus));
 }
 
-cartProduct.innerHTML = localStorage.getItem("cart");
 
--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,51 +1,107 @@
 const taskInput = document.getElementById("task__input");
 const tasksAdd = document.getElementById('tasks__add');
 const tasksList = document.getElementById("tasks__list");
-
-tasksList.innerHTML = localStorage.getItem("key");
-const tasks = document.querySelectorAll(".task");
+let taskRemove = document.querySelectorAll(".task__remove");
 
 let task;
 
-function addText(event) {
+let mus = []
+let statusInit = initUserTaskList()
+let count;
+
+if(!statusInit) {
+    alert("Error bliat'")
+}
+
+tasksAdd.addEventListener("click", (event) => {
+    addText(event, mus);
+})
+
+drawTasks(mus)
+
+tasksList.addEventListener("click", removeTask)
+
+// Дальше только функции, за плейн код - дам пизды
+
+function initUserTaskList() {
+    let userTasks;
+
+    if (localStorage.length === undefined || localStorage.length === 0) {
+        return false;
+    }
+
+    let key = localStorage.getItem("key");
+    if (key === "" || key === null || key === undefined) {
+        return false
+    }
+
+    userTasks = JSON.parse(key);
+
+    if (userTasks === null || userTasks === undefined) {
+        return false
+    }
+
+    mus = userTasks;
+
+    return true
+}
+
+function drawTasks(tasks) {
+    if (tasks == null || tasks == undefined) {
+        return;
+    }
+
+    for (let i = 0; i < tasks.length; i++) {
+        task = document.createElement("div");
+        task.dataset.id = i;
+        task.className = 'task';
+        task.innerHTML = `
+            <div class="task__title">
+                ${tasks[i]}
+            </div>
+            <a href="#" class="task__remove">&times;</a>
+        `;
+        tasksList.appendChild(task);
+    }
+}
+
+function addText(event, mus) {
     event.preventDefault();
 
     if (!taskInput.value.trim()) {
         return;
     }
-
+    
+    mus.push(taskInput.value);
+    let musLength = mus.length-1;
     task = document.createElement("div");
+    task.dataset.id = musLength;
     task.className = 'task';
+   
     task.innerHTML = `
         <div class="task__title">
             ${taskInput.value}
         </div>
         <a href="#" class="task__remove">&times;</a>
-     `; 
+        `; 
+   
     tasksList.appendChild(task);
-
-    localStorage.setItem("key", tasksList.innerHTML); 
+    
+    taskInput.value = "";
+    console.log(mus)
+    let val = JSON.stringify(mus)
+    localStorage.setItem("key", val)
 }
 
- function removeText(elem) {
-    elem.querySelector(".task__remove").addEventListener("click", () => {
-        elem.remove();  
-        localStorage.setItem("key", tasksList.innerHTML);    
-    })
+function removeTask(event) {
+    let task =  event.target.closest(".task");
+    let taskDataId = task.dataset.id;
+         
+    task.remove();
+    mus.splice(taskDataId, 1)
+    
+    localStorage.setItem("key", JSON.stringify(mus));
 }
-
-tasksAdd.addEventListener("click", (event) => {
-    addText(event);  
-    removeText(task);   
-})
-
-for (let task of tasks) {
-    task.querySelector('.task__remove').addEventListener('click', () =>  {
-        task.remove();
-        localStorage.setItem("key", tasksList.innerHTML);
-    })
-}
-
 
 
 
